9c4d882d9a79b101f498d02c5d142bb5edfb95bd
Mongodbuser-red <andratatti@gmail.com>
2019-09-21 15:03:26 +0000

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -22,8 +22,7 @@ Hey {}, my name is {}! if you have any questions about how to use me please give
 For Help click /help
 The Manager Of This Bot [Hacktronian](tg://user?id={}).
 Supporter of This Bot @InFoTelGroup
-Want to add me to your chat? [Click here!](http://telegram.me/Cococola_bot?startgroup=botstart),
-"""
+Want to add me to your chat? [Click here!](http://telegram.me/Cococola_bot?startgroup=botstart),"""
 
 HELP_STRINGS = """
 
@@ -45,7 +44,7 @@ DONATE_STRING = """Heya, glad to hear you want to donate!
 It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen).DeadPool @Class_dex."""
+There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
@@ -137,7 +136,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN)
     else:
-        update.effective_message.reply_text("Hey How Are You! I am alive… ")
+        update.effective_message.reply_text("Hey! How are you? I am alive.")
 
 
 # for test purposes
